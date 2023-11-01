507a419a7bd12df9412cf7dca6befb07987361fe
Roger Bi <zhongjunbi@hotmail.com>
2023-11-01 19:33:08 +1100

    process

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -4,3 +4,10 @@ import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
 
 const app = new cdk.App();
 new CdkWorkshopStack(app, 'CdkWorkshopStack');
+
+
+// import * as cdk from 'aws-cdk-lib';
+// import { WorkshopPipelineStack } from '../lib/pipeline-stack';
+
+// const app = new cdk.App();
+// new WorkshopPipelineStack(app, 'CdkWorkshopPipelineStack');
