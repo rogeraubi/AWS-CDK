bcd7d40fee62e7df6aa576627aefc46671f7393e
Roger Bi <zhongjunbi@hotmail.com>
2023-11-01 20:34:46 +1100

    Add deploy stage to pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,22 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import { Construct } from 'constructs';
+import {WorkshopPipelineStage} from './pipeline-stage';
 import {CodeBuildStep, CodePipeline, CodePipelineSource} from "aws-cdk-lib/pipelines";
 
 export class WorkshopPipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+
         // This creates a new CodeCommit repository called 'WorkshopRepo'
         const repo = new codecommit.Repository(this, 'WorkshopRepo', {
             repositoryName: "WorkshopRepo"
         });
 
-        // The basic pipeline declaration. This sets the initial structure
+       // The basic pipeline declaration. This sets the initial structure
         // of our pipeline
        const pipeline = new CodePipeline(this, 'Pipeline', {
             pipelineName: 'WorkshopPipeline',
             synth: new CodeBuildStep('SynthStep', {
-                    input: CodePipelineSource.codeCommit(repo, 'master'),
+                    input: CodePipelineSource.codeCommit(repo, 'main'),
                     installCommands: [
                         'npm install -g aws-cdk'
                     ],
@@ -28,5 +30,8 @@ export class WorkshopPipelineStack extends cdk.Stack {
                 }
             )
         });
+
+        const deploy = new WorkshopPipelineStage(this, 'Deploy');
+        const deployStage = pipeline.addStage(deploy);
     }
 }
